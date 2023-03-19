20a80e4ef355be37ff8676ef45e98be3155d185a
ivan retamozo <retamozoi445@gmail.com>
2023-03-19 19:51:32 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: 'alert',
+    loadChildren: () =>
+      import('./pages/alert/alert.module').then((m) => m.AlertPageModule),
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () =>
+      import('./pages/action-sheet/action-sheet.module').then(
+        (m) => m.ActionSheetPageModule
+      ),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ComponentItem } from 'src/app/core/interfaces/component.interface';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage {
+  componentList: ComponentItem[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'alert-outline',
+      name: 'Alert',
+      redirectTo: '/alert',
+    },
+  ];
+}
